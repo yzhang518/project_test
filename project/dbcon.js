6150449b73421447be3ea289c247a92f0aadd667
yzhang518 <59582407+yzhang518@users.noreply.github.com>
2021-02-23 22:16:26 -0500

    Update dbcon.js

--- project/dbcon.js
+++ project/dbcon.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : 'cs340_zhangyin',
-  password        : '6395',
-  database        : 'cs340_zhangyin'
+  user            : 'faegewf',
+  password        : 'gskgjergj;ag',
+  database        : 'afewgaweg'
 });
 module.exports.pool = pool;
