6890f8342e173ee547b7b813551ce14a32e40a67
Ren Demeis-Ortiz <ldemeisortiz@gmail.com>
2021-03-07 23:15:45 -0500

    Added alert to Authors and Members pages.

--- project/warnings.js
+++ project/warnings.js
@@ -1,11 +1,9 @@
 const author_book = {
-	1452: 'No such author exists.  Please verify author first and last name on author page and try again.',
 	1062: 'The author you entered is already listed for that title.',
 	1048: 'No such book title or author exists. Please verify book title and author name on the books and authors pages and try again.'
 };
 
 const category_book = {
-	1452: 'No such category exists.  Please verify category name on category page and try again.',
 	1062: 'The category you entered is already listed for that title.',
 	1048: 'No such book title or category exists. Please verify book title and category name on the books and categories pages and try again.'
 };
@@ -15,16 +13,24 @@ const book = {
 	1048: 'No such author or category exists. Please verify author name and category name on the authors and categories pages and try again.'
 };
 
-const category = {
-	1062: 'The category name you entered already exists.',
+const err1048 = {
+	author_book: 'No such book title or author exists. Please verify book title and author name on the books and authors pages and try again.',
+	category_book: 'No such book title or category exists. Please verify book title and category name on the books and categories pages and try again.',
+    book: 'No such author or category exists. Please verify author name and category name on the authors and categories pages and try again.'
 };
 
-const author = {
-	1062: 'The author first and last name combination you entered already exists.',
+function message(errno, type){
+	if(errno === 1062){
+		warning = 'The '+type+' you entered already exists.';
+	}
+	else if(errno===1048){
+		warning = err1048[type];
+	}
+	return warning;
+	
 };
 
 module.exports.author_book = author_book;
 module.exports.category_book = category_book;
 module.exports.book = book;
-module.exports.category = category;
-module.exports.author = author;
+module.exports.message = message;
